8962c22aa8e070594af6168531f921da87cde554
João Victor <joaovba2002@alu.ufc.br>
2022-12-16 16:52:09 -0300

    Ceará, Limites estacionários e municípios

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -1,18 +1,32 @@
-import "leaflet/dist/leaflet.css"
-import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import { TileLayer, MapContainer, GeoJSON } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import Style from "../../styles/Home.module.css"
-import data from "../../data/fortaleza"
+import ceara from "../../data/ceara.json"
+import bairros from "../../data/fortaleza-bairros.json"
+import municipios from "../../data/municipios.json"
+import limites from "../../data/limites-sensitarios.json"
 
 const Map = () => {
     return (
-        <MapContainer className={Style.Map} center={[-3.728272, -38.526316]} zoom={12} scrollWheelZoom={true}>
+        <MapContainer
+            className={Style.Map}
+            zoom={6.5} // initial zoom required
+            preferCanvas
+            maxZoom={20} // required
+            minZoom={3} // required
+            center={[-5.2, -38.5434]}
+        // Other map props...
+        >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <GeoJSON data={data} />
+             <GeoJSON data={ceara} />
+             {/* <GeoJSON data={bairros} /> */}
+             <GeoJSON data={municipios} />
+             <GeoJSON data={limites} />
         </MapContainer>
     );
-}
+};
 
-export default Map
+export default Map;
