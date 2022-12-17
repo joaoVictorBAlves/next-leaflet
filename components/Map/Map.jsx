44ea9e7a8d27f87b67136aeef584eee50120633e
João Victor <joaovba2002@alu.ufc.br>
2022-12-17 14:08:53 -0300

    Change color and click in city

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -1,12 +1,31 @@
 import { TileLayer, MapContainer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import Style from "../../styles/Home.module.css"
+import Style from "../../styles/Map.module.css"
 import ceara from "../../data/ceara.json"
 import bairros from "../../data/fortaleza-bairros.json"
 import municipios from "../../data/municipios.json"
 import limites from "../../data/limites-sensitarios.json"
 
 const Map = () => {
+    const citiesStyle = {
+        fillColor: "red",
+        fillOpacity: 1,
+        color: "black",
+        weight: 1,
+    };
+
+    const handdleOnEachCity = (city, layer) => {
+        const name = city.properties.name
+        const value = city.properties.value
+
+        layer.bindPopup(name + " - value " + value)
+        layer.on({
+            click: (event) => {
+              console.log(event)  
+            },
+        })
+    }
+
     return (
         <MapContainer
             className={Style.Map}
@@ -17,14 +36,14 @@ const Map = () => {
             center={[-5.2, -38.5434]}
         // Other map props...
         >
-            <TileLayer
+            {/* <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-             <GeoJSON data={ceara} />
-             {/* <GeoJSON data={bairros} /> */}
-             <GeoJSON data={municipios} />
-             <GeoJSON data={limites} />
+            /> */}
+            {/* <GeoJSON data={bairros} /> */}
+            {/* <GeoJSON  data={ceara} /> */}
+            <GeoJSON style={citiesStyle} data={municipios} onEachFeature={handdleOnEachCity} />
+            {/* <GeoJSON data={limites} /> */}
         </MapContainer>
     );
 };
